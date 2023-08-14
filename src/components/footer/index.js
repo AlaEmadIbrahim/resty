10bd427ab0d93fbf30d5e8cbfb3250202e9f43eb
AlaEmadIbrahim <ALA.EMAD.IBRAHIM@GMAIL.COM>
2023-08-14 14:57:13 +0300

    done lab26

---
+++ src/components/footer/index.js
@@ -0,0 +1,10 @@
+import React from "react";
+import "./footer.scss";
+
+class Footer extends React.Component {
+  render() {
+    return <footer>&copy; 2023</footer>;
+  }
+}
+
+export default Footer;
